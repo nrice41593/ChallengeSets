a177c6764c936f9cdcafe09dd487e1629bd6d657
nate <nrice@truecoders.io>
2019-11-11 17:40:58 -0600

    added and tested Challenge Set 7 with tests

---
+++ ChallengeSets/ChallengeSet07.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChallengeSets
+{
+    public class ChallengeSet07
+    {
+        public int CountOfBusinessesWithNegativeNetProfit(List<Business> businesses)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCommaSeparatedListOfProfitableBusinesses(List<Business> businesses)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetNameOfHighestParentCompany(Business business)
+        {
+            // If there is Company A, whose parent is Company B, whose parent is Company C, then given Company A return Company C
+            throw new NotImplementedException();
+        }
+
+        public enum TicTacToeResult { X, O, Draw }
+        public TicTacToeResult GetTicTacToeWinner(char[,] finalBoard)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EachArrayInJaggedArrayContainsTargetNumber(int[][] numbers, int targetNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
