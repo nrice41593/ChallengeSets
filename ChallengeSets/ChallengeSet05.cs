cc36fb126f6e2211cddc2f1c8e97c742d279bf54
nate <nrice@truecoders.io>
2019-11-11 17:30:44 -0600

    added Challenge Set 5 with tests

---
+++ ChallengeSets/ChallengeSet05.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChallengeSets
+{
+    public class ChallengeSet05
+    {
+        public int GetNextNumberDivisibleByN(int startNumber, int n)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsAscendingOrder(int[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SumElementsThatFollowAnEven(int[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string TurnWordsIntoSentence(string[] words)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double[] GetEveryFourthElement(List<double> elements)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TwoDifferentElementsInArrayCanSumToTargetNumber(int[] nums, int targetNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
