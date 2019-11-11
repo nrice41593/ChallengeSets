1b47831c00b1d1a036b2ab28015f672cfd6bc618
nate <nrice@truecoders.io>
2019-11-11 16:52:40 -0600

    added first challenge set with tests

---
+++ ChallengeSets/ChallengeSet01.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace ChallengeSets
+{
+    public class ChallengeSet01
+    {
+        public bool AreTwoNumbersTheSame(int num1, int num2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Subtract(double minuend, double subtrahend)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Add(int number1, int number2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetSmallestNumber(int number1, int number2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long Multiply(long factor1, long factor2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetGreeting(string nameOfPerson)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetHey()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
