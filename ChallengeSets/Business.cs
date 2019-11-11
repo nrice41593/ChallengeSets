c6e6cd97d909b7ac66bc054188e8486605d68de4
nate <nrice@truecoders.io>
2019-11-11 17:20:49 -0600

    added business class

---
+++ ChallengeSets/Business.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ChallengeSets
+{
+    public class Business
+    {
+        public string Name { get; set; }
+        public double TotalRevenue { get; set; }
+        public double TotalExpenses { get; set; }
+        public Business ParentCompany { get; set; }
+        public int Length { get; internal set; }
+
+        internal string ToString(object p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
